d3bd6ddae243b9c234b219906b0a7ce98fde72b4
yenifersan <44785957+yenifersan@users.noreply.github.com>
2019-11-11 16:25:38 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio11/Ejercicio11/ITextToSpeech.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio11
+{
+    public interface ITextToSpeech
+    {
+        void Speak(string text);
+    }
+}
+
---
+++ Ejercicio11/Ejercicio11/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio11
+{
+    public class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        public bool Done { get; set; }
+    }
+}
+
